c7aac36aee3670ab708026002527fe6e68c7bdf8
Sven Rosema <rosema.sven@gmail.com>
2026-05-09 09:28:15 +0200

    fix(vehicles): add VehicleUpdate convenience alias for symmetry with Insert

--- webapp/src/types/database.ts
+++ webapp/src/types/database.ts
@@ -919,6 +919,7 @@ export type NotificationLog = Database["public"]["Tables"]["notification_logs"][
 // Vehicle types
 export type Vehicle = Database["public"]["Tables"]["vehicles"]["Row"];
 export type VehicleInsert = Database["public"]["Tables"]["vehicles"]["Insert"];
+export type VehicleUpdate = Database["public"]["Tables"]["vehicles"]["Update"];
 
 // Recipe instruction type
 export interface RecipeInstruction {
